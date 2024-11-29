26442eb4240f2cecf91e71406c72b685912cb415
Raul Albín <raul200794@gmail.com>
2024-11-29 22:56:15 +0100

    Quick Sort

---
+++ src/main/kotlin/MainQuickSort.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainQuickSort {
+    public static void main(String[] args) {
+        List<Integer> integers = new ArrayList<>(){{
+            add(9);
+            add(3);
+            add(15);
+            add(9);
+            add(1);
+            add(0);
+            add(5);
+            add(13);
+            add(18);
+        }};
+        System.out.println(integers);
+        QuickSorter.quickSort(integers);
+
+        System.out.println(integers);
+    }
+}
