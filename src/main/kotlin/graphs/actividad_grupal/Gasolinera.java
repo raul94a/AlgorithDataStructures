ce8212eb891ca40ae40a1d80a5fa8bb4066bb227
raul.albin <raul200794@gmail.com>
2025-03-09 22:03:18 +0100

    Got Dijkstra Algorithm implemented

---
+++ src/main/kotlin/graphs/actividad_grupal/Gasolinera.java
@@ -0,0 +1,32 @@
+package graphs.actividad_grupal;
+
+public class Gasolinera implements NodoGrafo{
+    private String clave;
+    private String rotulo;
+    private String direccion;
+    private String municipio;
+    private String localidad;
+
+    public Gasolinera(String clave, String rotulo, String direccion, String municipio, String localidad) {
+        this.clave = clave;
+        this.rotulo = rotulo;
+        this.direccion = direccion;
+        this.municipio = municipio;
+        this.localidad = localidad;
+    }
+
+
+    @Override
+    public String toString() {
+        return "[clave=" + clave + ", rotulo=" + rotulo + ", direccion=" + direccion + ", municipio="
+                + municipio + ", localidad=" + localidad + "]";
+    }
+
+
+    @Override
+    public String getClave() {
+
+        return clave;
+    }
+
+}
---
+++ src/main/kotlin/graphs/actividad_grupal/Main.java
@@ -0,0 +1,60 @@
+package graphs.actividad_grupal;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.charset.Charset;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // DECLARACIÓN DEL GRAFO
+        Grafo grafo = new Grafo();
+
+        // LECTURA DE LOS DATOS DE LAS GASOLINERAS Y CONSTRUCCIÓN DE LOS NODOS DEL GRAFO
+        String directorioProyecto = System.getProperty("user.dir");
+        String nombreFicheroGasolineras = "gasolineras.csv";
+        String separador = System.getProperty("file.separator");
+        String pathFicheroGasolineras = directorioProyecto + separador + nombreFicheroGasolineras;
+
+        String[] datosGasolinera = new String[5];
+        Gasolinera gasolinera;
+
+        try (FileReader fr = new FileReader(pathFicheroGasolineras, Charset.forName("ISO-8859-1"))) {
+            BufferedReader br = new BufferedReader(fr);
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                datosGasolinera = linea.split(";");
+                gasolinera = new Gasolinera(datosGasolinera[2], datosGasolinera[4], datosGasolinera[3], datosGasolinera[0], datosGasolinera[1]);
+                grafo.insertarNodo(gasolinera);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+        // LECTURA DE LAS DISTANCIAS ENTRE GASOLINERAS Y CONSTRUCCIÓN DE LAS ARISTAS DEL GRAFO
+        String nombreFicheroDistanciasGasolineras = "distancias-gasolineras.csv";
+        String pathFicheroDistanciasGasolineras = directorioProyecto + separador + nombreFicheroDistanciasGasolineras;
+        String[] datosDistanciaGasolineras;
+
+        try (FileReader fr = new FileReader(pathFicheroDistanciasGasolineras, Charset.forName("ISO-8859-1"))) {
+
+            BufferedReader br = new BufferedReader(fr);
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+                datosDistanciaGasolineras = linea.split(";");
+                grafo.insertarArista(datosDistanciaGasolineras[0], datosDistanciaGasolineras[1], Double.parseDouble(datosDistanciaGasolineras[2]));
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+        System.out.println(grafo);
+
+    }
+
+}
